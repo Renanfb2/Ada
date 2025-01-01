633a3feb9ec00d38fb9489fc25ba5cc5e430e40d
Renanfb2 <renanfb2@gmail.com>
2025-01-01 20:21:50 -0300

    Tipos String e Boolean

--- Node.js/Aula-01/arquivo.js
+++ Node.js/Aula-01/arquivo.js
@@ -1,9 +1,15 @@
-let numero = 20
+let numero = 20 // tipo inteiro
 console.log(numero)
 
-numero = 7
+numero = 7.5 // tipo real
 console.log(numero)
 
-const nome = "Renan"
+const nome = "Renan" // tipo String
 
 console.log(`Oi, ${nome}!`)
+
+const EstaChovendo = false // tipo bolleano
+
+console.log(`O clima é chuvoso? ${EstaChovendo}`)
+
+console.log(teste)
