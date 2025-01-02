ad1490a7e117b44faf53627d7f0361837c7ad728
Renanfb2 <renanfb2@gmail.com>
2025-01-02 18:34:51 -0300

    exemplos de while

--- Node.js/Aula-01/arquivo7.js
+++ Node.js/Aula-01/arquivo7.js
@@ -1,6 +1,6 @@
-// LOOPS
-// 1) while
-const nomesDeUsiarios = ["Pedro", "Tiago", "João", "Marcos"]
+// While
+// 1) impressão de nomes de uma lista
+const nomesDeUsuarios = ["Pedro", "Tiago", "João", "Marcos"]
 
 // Para imprimir uma msg para cada usuário fazemos assim: (maneira "burra")
 // console.log("Olá,", nomesDeUsiarios[0])
@@ -11,7 +11,49 @@ const nomesDeUsiarios = ["Pedro", "Tiago", "João", "Marcos"]
 // Isto pode ser simplificado com o uso do while, desta forma
 let indice = 0
 
-while (indice < nomesDeUsiarios.length) { // método .length informa o tamanho do array
-    console.log("Olá,", nomesDeUsiarios[indice], {indice})
+while (indice < nomesDeUsuarios.length) { // método .length informa o tamanho do array
+    console.log("Olá,", nomesDeUsuarios[indice], {indice})
     indice += 1
 }
+
+// 2) Separação de Pares e Impares
+const numeros = [25, 88, 71, 192, 333, 222, 10, 11, 62]
+const pares = []
+const impares = []
+let index = 0
+
+console.log(numeros)
+while (index < numeros.length) {
+    if (numeros[index] % 2 === 0) {
+        pares.push(numeros[index])
+    }
+    else {
+        impares.push(numeros[index])
+    }
+    index +=1
+}
+console.log(pares)
+console.log(impares)
+
+// 3) Encontrar um nome específico em uma lista
+const contatos = ["Pedro", "Tiago", "João", "André", "Simão", "Bartolomeu", "Felipe", "Mateus", "Tomé"]
+let encontrouNome = false
+let fimDaLista = false
+let indexContato = 0
+
+while (!encontrouNome && !fimDaLista) {
+    let contatoAtual = contatos[indexContato] // fez assim para facilitar a visualização, pois poderia ter jogado o contatos[indexContato] diretamente no if abaixo, mas poderia parecer que o método acessado era da lista, quando na verdade é de uma string que é o nome do contato.
+    if (contatoAtual.startsWith("B")){ // strings também possuem diversos métodos que podem ser acessados
+        encontrouNome = true
+        console.log(`Contato encontrado: ${contatoAtual}.`)
+    }
+    indexContato += 1
+
+    if (indexContato === contatos.length) {
+        console.log("Contato não foi encontrado")
+        fimDaLista = true
+    }
+}
+
+// Existe o comamdo "break" que interrompe um while independente da condição ter sido ou não atingida.
+// Caso haja um ninho de whiles, o break so interrompe o while onde ele está colocado, mas não os whiles superiores.
