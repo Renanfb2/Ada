b3a066c3c794de2a8cd7817cdfe8e95649170909
Renanfb2 <renanfb2@gmail.com>
2025-01-01 21:33:22 -0300

    Operadores Matemáticos

---
+++ Node.js/Aula-01/arquivo3.js
@@ -0,0 +1,26 @@
+// operadores matemáticos
+const num1 = 10
+const num2 = 30
+const num3 = 1
+const num4 = 2
+
+
+const soma = num1 + num2
+const subtracao = num2 - num1
+const multiplicacao = num1 * num2
+const divisao = num2 / num1
+const potencia = num4 ** num3
+const resto = num3 % num4 // funciona para uma divisão de 2 numeros inteiros
+const potencia2 = Math.pow(num1, num4) // existe uma biblioteca chamada Math com inumeras funções matemáticas
+const negativo = num1 - num2
+const modulo = Math.abs(negativo)
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
+console.log(potencia)
+console.log(resto)
+console.log(potencia2)
+console.log(negativo)
+console.log(modulo)
