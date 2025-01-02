f368ea1523d349e9047e42a6513bc33043bbbb08
Renanfb2 <renanfb2@gmail.com>
2025-01-01 21:57:34 -0300

    Arrays ou vetores/listas

---
+++ Node.js/Aula-01/arquivo4.js
@@ -0,0 +1,6 @@
+// Listas, Vetores ou Arrays
+const alunos = ["Joana", "Cássia", "Kevin"]
+console.log(alunos)
+
+const lista_diversa = ["pedro", "paulo", 5, false, 3.23, ["claudio, 11, true"]]
+console.log(lista_diversa)
