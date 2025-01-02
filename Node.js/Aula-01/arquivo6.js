79f6283f87683b23444d59d96f1508bc13ee023e
Renanfb2 <renanfb2@gmail.com>
2025-01-02 16:30:35 -0300

    If-Else, Ternários

--- Node.js/Aula-01/arquivo6.js
+++ Node.js/Aula-01/arquivo6.js
@@ -1,8 +1,24 @@
-// ternários
+// Atribuição condicional
 const idade = 19
 const temCNH = true
-const podeDirigir = idade > 18 && temCNH
+const podeDirigir = idade > 18 && temCNH // atribuição feita a podeDirigir está condicionada ao resultado das 2 comparações das variáveis idade e temCNH
 
 if (podeDirigir) {
     console.log("Pessoa está habilitada para dirigir")
+}   
+else {
+    console.log("Pessoa não está habilitada para dirigir")
 }
+
+// Ternário
+// vamos refazer o código acima, simplificando a atribuição da variável numPassageiros
+const numPassageiros = podeDirigir ? 4 : 0 // neste caso ele atribui 4 se pode dirigir for true e 0 se for false. Este ternário tem o mesmo efeito de fazer com if, que fica um código maior, assim:
+
+// let numPassageiros = 0
+// if (podeDirigir) {
+//    numPassageiros = 4
+//}
+//else {
+//    numPassageiros = 0
+//}
+console.log({ numPassageiros }) // quando coloco a variável dentro de {}, indico que é um "objeto" e ele retorna o nome da variável e o valor dela -> { numPassageiros: 4 }, facilitando a identificação do resultado
